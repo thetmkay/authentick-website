5de3297e7a523a18aa04744bf29e6bb3108bee78
thetmkay <george@upstatement.com>
2014-06-08 09:03:10 -0400

    twitter search demo

--- public/js/app.js
+++ public/js/app.js
@@ -2,4 +2,110 @@
 
 	'use strict';
 
+	$(document).ready( function() {
+		var index = 1,
+			filter = 'tweet';
+		// var ts = {
+		// 	q:"%23newshack",
+		// 	geocode: escape("42.358283,-71.093425,1mi"),
+		// 	type:escape("recent"),
+		// 	count : 100
+		// },
+		// html_elem = "<div></div>";
+
+		// var twq = ts.q + "&geocode=" + ts.geocode + "&result_type=" + ts.type + "&count=" + ts.count;
+		// console.log(twq);
+
+		var container_elem = "<div class='tweet-container'><div class='tweet-author'><a class='tweet-author-link' href='https://twitter.com/tweetauthor' target='_blank'>tweetauthor</a></div>",
+			tweet_elem = "<div class='tweet'><div class='tweet-body'>tweettext</div></div>";
+
+		$("#add-plus").on("click", function(event) {
+			$(this).before("<input class='filter-and-or-" + index + "' type='button' name='filter-name-'" + index + " value='&'><input placeholder='#another' class='filter-text' type='text' name='filter-text-" + index + "'>");
+			$('.filter-and-or-' + index).on('click', function(event) {
+				if($(this).val() === '&') {
+					$(this).val('or');
+				} else if($(this).val() === 'or') {
+					$(this).val('&');
+				}
+			});
+			index++;
+		});
+
+		$(".radio-button").on('click', function(event) {
+			event.preventDefault();
+			$(".radio-button[data-checked=true]").attr("data-checked", "false");
+			$(this).attr("data-checked", "true");
+			filter = $(this).val();
+			console.log(filter);
+		});
+
+		$("#twitter-search").on("submit", function(event) {
+			event.preventDefault();
+			var ts = {
+				type: escape("recent"),
+				count: 100
+			};
+			$(this).children('input:not([type=submit])').each(function(index,element) {
+				ts[$(element).attr('name')] = $(element).val();
+			});
+
+			var and_filters = [],
+				or_filters = [];
+
+			$('.filter-text').each(function(index,element) {
+				var operator = $(this).prev().val();
+				if(operator === '&') {
+					and_filters.push($(this).val());
+				} else if (operator === 'or') {
+					or_filters.push($(this).val());
+				}
+			});
+
+			console.log(and_filters);
+			console.log(or_filters);
+
+			console.log(ts);
+			var twq = escape(ts.query) + "/" + escape(ts.address) + "/" + escape(ts.radius) + "/" + ts.type + "/" + ts.count;
+			console.log(twq);
+			$.get('/get/' + twq, function(users,err) {
+				// console.log(data)
+				$.each(users,function(index,user) {
+					console.log(user);
+					var elem_string = container_elem.replace('tweetauthor', index).replace('tweetauthor', index);
+
+					for(var username in user) {
+						var element = user[username];
+						console.log('/gettweet/' + element.id);
+						console.log(element);
+						var tweet_string = tweet_elem.replace('tweettext',element.text);
+						var mentions = element.entities.user_mentions;
+						for(var i in mentions) {
+							console.log(i);
+							var handle = '@' + mentions[i]
+							tweet_string.replace(handle, '<a href="https://www.twitter.com/'+mentions[i]+'">'+handle+'</a>')
+						}
+						console.log(element.entities.user_mentions);
+						// for (var index in element.entities.user_mentions) {
+						// 	var element = element.entities.user_mentions[index];
+						// 	console.log(index);
+						// 	elem_string = elem_string.replace('@'+element.screen_name,'<a href="https://www.twitter.com/'+element.screen_name+'" target="_blank">'+'@'+element.screen_name+'</a>');
+						// }
+						console.log(elem_string);
+						elem_string += tweet_string;
+					}
+					elem_string += "</div>";
+					$('body').append(elem_string);
+				});
+
+			});
+		});
+
+
+	});
+
+
+
+
+
+
 })(jQuery);
--- server.js
+++ server.js
@@ -3,10 +3,24 @@
  */
 
 var express = require('express'),
-  http = require('http'),
-  path = require('path');
+	cookieParser = require('cookie-parser'),
+	session      = require('express-session'),
+ 	http = require('http'),
+  	path = require('path'),
+  	passport = require('passport'),
+  	TwitterStrategy = require('passport-twitter').Strategy,
+  	request = require('request'),
+  	_ = require('underscore'),
+  	oauth = {
+      consumer_key    : "5GfhKfFzAS1vG40TjLUNLUIbk",
+      consumer_secret : "MPKDmHCLnZRQ9ExBu1c6fS0rdBC7B6NkqIrw9SkEJUzgKjGj82",
+      token           : "159273941-CZUKmd3zd8qtQNjZmwJH8WhxliqnkmKY8bRRopvQ",//req.session['oauth:twitter']['oauth_token'],
+      token_secret    : "j2aO0CXJbMVXKkO0AST4dyQTZrtaQoUerv8fXLuFxVYE0"//req.session['oauth:twitter']['oauth_token_secret']
+    };
 
 var app = module.exports = express();
+
+var this_token, this_tokenSecret;
 
 /**
  * Configuration
@@ -15,14 +29,162 @@ var app = module.exports = express();
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(cookieParser()) // required before session.
+app.use(session({
+    secret: 'keyboard cat'
+  , proxy: true // if you do SSL outside of node.
+}))
+app.use(passport.initialize());
+app.use(passport.session());
 // development only
 
+passport.use(new TwitterStrategy({
+    consumerKey: oauth.consumer_key,
+    consumerSecret: oauth.consumer_secret,
+    callbackURL: "http://localhost:3000/"
+  },
+  function(req,token, tokenSecret, profile, done) {
+    var oauth = {
+      consumer_key    : "5GfhKfFzAS1vG40TjLUNLUIbk",
+      consumer_secret : "MPKDmHCLnZRQ9ExBu1c6fS0rdBC7B6NkqIrw9SkEJUzgKjGj82",
+      token           : token,
+      token_secret    : tokenSecret
+    }
 
+    var url = 'https://api.twitter.com/1.1/search/tweets.json?q=%23newshack';
+    console.log(req);
+    request.get({url:url, oauth:oauth, json:true}, function (e, r, result) {
+    	console.log(result);
+    });
+  	done(null, false);
+  }
+));
 
 /**
  * Routes
  */
+
+app.get('/auth/twitter', passport.authenticate('twitter'));
+
+app.get('/auth/twitter/callback', passport.authenticate('twitter', { successRedirect: '/'}));
+
+app.get('/get/:query/:address/:radius/:type/:count', function(req,res) {
+
+	var url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + req.params.address + "&key=AIzaSyAtxKiAoIx4xZeJsbP0G8S8vQ8f89e6fjk";
+
+	request.get(url, function(e,r,geo_res) {
+		// console.log(geo_res[0]);
+		var geo_json = JSON.parse(geo_res);
+		// console.log(geo_json.results);
+		// for(var prop in geo_res) {
+		// 	console.log(prop);
+		// }
+		var loc = geo_json.results[0].geometry.location;
+		// console.log(loc);
+	    url = 'https://api.twitter.com/1.1/search/tweets.json?q=' + escape(req.params.query)
+	    	+ "&geocode=" + loc.lat +"," + loc.lng + "," + req.params.radius + "mi"
+	    	+ "&result_type=" + req.params.type
+	    	+ "&count=" + req.params.count;
+	    console.log(url);
+	    request.get({url:url,oauth:oauth,json:true}, function(e,r,result) {
+	    	var statuses = result.statuses;
+	    	console.log(statuses);
+	    	var search_result = _.groupBy(statuses, function(status) {
+	    		return status.user.screen_name;
+	    	});
+	    	console.log(search_result);
+	    	res.json(search_result);
+	    });
+	});
+});
+
+app.get('/gettweet/:id', function(req,res) {
+	console.log(req.params.id);
+    var url = 'https://api.twitter.com/1/statuses/oembed.json?id=' + req.params.id;
+
+    request.get({url:url,oauth:oauth,json:true}, function(e,r,result) {
+    	// console.log(result);
+    	res.json(result);
+    });
+});
+
+// app.get('/get', function(req,res) {
+
+// 	var oauth = {
+//       consumer_key    : configAuth.twitterAuth.consumerKey,
+//       consumer_secret : configAuth.twitterAuth.consumerSecret,
+//       token           : token,
+//       token_secret    : tokenSecret
+//     };
+
+//     var url = 'https://api.twitter.com/1.1/users/show.json?';
+
+//     request.get({url:url,oauth:oauth,json:true}, function(e,r,result) {
+
+//     });
+
+// 	// request.post({url:'https://api.twitter.com/oauth2/token',body:'grant_type=client_credentials'}, function(e,r,body) {
+// 	// 	console.log(body);
+// 	// });
+
+// 	// console.log(req);
+// 	// console.log(this_token);
+// 	// console.log(this_tokenSecret);
+// 	// var oauth =
+// 	//         { consumer_key: "5GfhKfFzAS1vG40TjLUNLUIbk"
+// 	//         , consumer_secret: "MPKDmHCLnZRQ9ExBu1c6fS0rdBC7B6NkqIrw9SkEJUzgKjGj82"
+// 	//         , token: this_token
+// 	//         , token_secret: this_tokenSecret
+// 	//         };
+//  //    request.get({url:'https://api.twitter.com/1.1/search/tweets.json?q=%23newshack', oauth:oauth, json:true}, function (e, r, user) {
+//  //      console.log(user)
+//  //    });
+// })
+
+// app.get('/get', function(req,res) {
+// 	var qs = require('querystring')
+// 	  , oauth =
+// 	    { callback: 'http://localhost:3000/callback/'
+// 	    , consumer_key: "5GfhKfFzAS1vG40TjLUNLUIbk"
+// 	    , consumer_secret: "MPKDmHCLnZRQ9ExBu1c6fS0rdBC7B6NkqIrw9SkEJUzgKjGj82"
+// 	    }
+// 	  , url = 'https://api.twitter.com/oauth/request_token'
+// 	  ;
+// 	request.post({url:url, oauth:oauth}, function (e, r, body) {
+// 	  // Ideally, you would take the body in the response
+// 	  // and construct a URL that a user clicks on (like a sign in button).
+// 	  // The verifier is only available in the response after a user has
+// 	  // verified with twitter that they are authorizing your app.
+// 	  var access_token = qs.parse(body)
+// 	    , oauth =
+// 	      { consumer_key: CONSUMER_KEY
+// 	      , consumer_secret: CONSUMER_SECRET
+// 	      , token: access_token.oauth_token
+// 	      , verifier: access_token.oauth_verifier
+// 	      }
+// 	    , url = 'https://api.twitter.com/oauth/access_token'
+// 	    ;
+// 	  request.post({url:url, oauth:oauth}, function (e, r, body) {
+// 	    var perm_token = qs.parse(body)
+// 	      , oauth =
+// 	        { consumer_key: CONSUMER_KEY
+// 	        , consumer_secret: CONSUMER_SECRET
+// 	        , token: perm_token.oauth_token
+// 	        , token_secret: perm_token.oauth_token_secret
+// 	        }
+// 	      , url = 'https://api.twitter.com/1.1/users/show.json?'
+// 	      , params =
+// 	        { screen_name: perm_token.screen_name
+// 	        , user_id: perm_token.user_id
+// 	        }
+// 	      ;
+// 	    url += qs.stringify(params)
+// 	    request.get({url:url, oauth:oauth, json:true}, function (e, r, user) {
+// 	      console.log(user)
+// 	    })
+// 	  })
+// 	})
+// });
 
 app.get('/:name', function(req,res) {
   res.sendfile(req.params.name);
